74f76e8f67dc5cb6ff89c520fa66b2172d05495d
Talon Daniels <talondaniels@gmail.com>
2013-11-06 05:12:03 -0700

    new level generation

--- core/src/main/java/org/nolat/explosions/LevelInfo.java
+++ core/src/main/java/org/nolat/explosions/LevelInfo.java
@@ -7,29 +7,45 @@ import java.util.List;
 import com.badlogic.gdx.utils.Array;
 
 public class LevelInfo {
-    private static float PER_CENT_IGNORE = 0.93f;
+    private static float PER_CENT_IGNORE = 0.935f;
     private static HashMap<Integer, LevelInfo> levelData = new HashMap<>();
 
     static {
         Array<LevelInfo> tmp = new Array<LevelInfo>();
         LevelInfo info = new LevelInfo(3, 4);
-        for (int i = -4; info.numNeededToPass > 2; i--) {
+        for (int i = 0; info.numTotal <= 70; i++) {
             info = LevelInfo.generateLevelInfo(i);
             tmp.add(info);
         }
-        tmp.reverse();
-        for (int i = 0; i < tmp.size; i++) {
-            tmp.get(i).setLevel(i);
+        //        tmp.reverse();
+        //always add 0th level
+        tmp.get(0).setLevel(0);
+        levelData.put(0, tmp.get(0));
+
+        int lvl = 1;
+        for (int i = 1; i < tmp.size; i++) {
             float perCent = (float) tmp.get(i).numNeededToPass / (float) tmp.get(i).numTotal;
             if (perCent < PER_CENT_IGNORE) {
-                levelData.put(i, tmp.get(i));
+
+                if (!tmp.get(i - 1).equals(tmp.get(i))) {
+                    tmp.get(i).setLevel(lvl);
+                    levelData.put(lvl, tmp.get(i));
+                    lvl++;
+                } else {
+                    System.out.println("Discarded " + tmp.get(i) + " for being a duplicate");
+                }
+
             } else {
                 if (Config.debug) {
                     System.out.println("Discarded " + tmp.get(i) + " for having " + perCent);
                 }
             }
-            if (Config.debug) {
-                System.out.println(tmp.get(i));
+        }
+
+        if (Config.debug) {
+            System.out.println("\n--Levels--");
+            for (LevelInfo li : getAllLevels()) {
+                System.out.println(li);
             }
         }
     }
@@ -43,9 +59,11 @@ public class LevelInfo {
     }
 
     public static LevelInfo generateLevelInfo(int x) {
-        // e^(-(x/20)^2)*65+1 ; e^(-(x/27)^2)*65+2
-        int needed = (int) Math.round((Math.pow(Math.E, -Math.pow(x / 15.0, 2.0)) * 60f + 1f));
-        int total = (int) Math.round((Math.pow(Math.E, -Math.pow(x / 20.0, 2.0)) * 65f + 2f));
+        // I've gone insane: http://www.wolframalpha.com/input/?i=63-63*e%5E%28-%28%28x%2B5%29%2F30%29%5E2%29+%2B+4*%280.25*cos%280.65*x%29%29+%2B+0+and+x%3D70+and+x+and+63-63*e%5E%28-%28%28x%2B5%29%2F30%29%5E2%29+%2B+5*%280.45*sin%280.65*x%29%29+%2B+7+from+0+to+65
+        // I'm definitely overcomplicating things
+        double e_equation = 63.0 * Math.pow(Math.E, -((1.0 / 900.0) * Math.pow(x + 5.0, 2.0)));
+        int needed = (int) Math.round(63.0 - e_equation + (1f * Math.cos(0.65f * x)));
+        int total = (int) Math.round(70.0 - e_equation + (2.25f * Math.sin(0.65f * x)));
         return new LevelInfo(needed, total);
     }
 
@@ -63,7 +81,8 @@ public class LevelInfo {
 
     private LevelInfo(int numNeededToPass, int numTotal) {
         if (numNeededToPass > numTotal) {
-            throw new IllegalArgumentException("Amount needed to pass must be less than or equal to the total.");
+            throw new IllegalArgumentException("Amount needed to pass must be less than or equal to the total: "
+                    + numNeededToPass + " is not less than " + numTotal);
         }
         level = levelData.size();
         this.numNeededToPass = numNeededToPass;
@@ -77,5 +96,11 @@ public class LevelInfo {
     @Override
     public String toString() {
         return "[" + level + ": " + numNeededToPass + " / " + numTotal + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        LevelInfo other = (LevelInfo) obj;
+        return numNeededToPass == other.numNeededToPass && numTotal == other.numTotal;
     }
 }

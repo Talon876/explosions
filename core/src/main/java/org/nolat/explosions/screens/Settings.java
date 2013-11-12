34307752c17fb2149091fb4e82e5207d2a9a38c1
Talon Daniels <talondaniels@gmail.com>
2013-11-12 01:06:44 -0700

    added ability to change name in Settings screen

--- core/src/main/java/org/nolat/explosions/screens/Settings.java
+++ core/src/main/java/org/nolat/explosions/screens/Settings.java
@@ -1,6 +1,7 @@
 package org.nolat.explosions.screens;
 
 import org.nolat.explosions.Config;
+import org.nolat.explosions.stackmob.Player;
 import org.nolat.explosions.utils.FontUtils;
 
 import com.badlogic.gdx.Game;
@@ -22,7 +23,11 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.stackmob.sdk.callback.StackMobCallback;
+import com.stackmob.sdk.exception.StackMobException;
 
 public class Settings implements Screen {
 
@@ -35,6 +40,10 @@ public class Settings implements Screen {
     private BitmapFont buttonFont;
     private BitmapFont titleFont;
 
+    private Player player;
+    private TextField nameField;
+    private TextButton save;
+
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
@@ -42,6 +51,7 @@ public class Settings implements Screen {
 
         stage.act(delta);
         stage.draw();
+        Table.drawDebug(stage);
     }
 
     @Override
@@ -68,19 +78,42 @@ public class Settings implements Screen {
 
         table = new Table(skin);
         table.setFillParent(true);
+        table.debug();
 
+        //fonts
         settingsFont = FontUtils.generateFont("fonts/square.ttf", 36, Color.WHITE);
         buttonFont = FontUtils.generateFont("fonts/square.ttf", 42, Color.WHITE);
         titleFont = FontUtils.generateFont("fonts/square.ttf", 90, Color.BLACK);
 
+        //styles
         LabelStyle labelStyle = skin.get("default", LabelStyle.class);
         labelStyle.font = titleFont;
         labelStyle.fontColor = Color.BLACK;
         skin.add("default", labelStyle, LabelStyle.class);
 
+        LabelStyle settingsLabelStyle = skin.get("settings", LabelStyle.class);
+        settingsLabelStyle.font = settingsFont;
+        settingsLabelStyle.fontColor = Color.BLACK;
+        skin.add("settings", settingsLabelStyle, LabelStyle.class);
+
+        TextFieldStyle textFieldStyle = skin.get("default", TextFieldStyle.class);
+        textFieldStyle.font = settingsFont;
+        skin.add("default", textFieldStyle, TextFieldStyle.class);
+
+        //settings items
+        nameField = new TextField("Please Wait...", skin);
+        nameField.setMaxLength(25);
+        nameField.setDisabled(true);
+
+        //settings tables
+        Table settingsTable = new Table(skin);
+        settingsTable.debug();
+        settingsTable.add("Highscore Name", "settings").pad(10f).left();
+        settingsTable.add(nameField).pad(10f).right().expand().fillX();
+
         TextButtonStyle buttonStyle = skin.get("default", TextButtonStyle.class);
         buttonStyle.font = buttonFont;
-        final TextButton save = new TextButton("Save", buttonStyle);
+        save = new TextButton("Save", buttonStyle);
         save.addListener(new ClickListener() {
             @Override
             public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
@@ -94,6 +127,10 @@ public class Settings implements Screen {
                 stage.addAction(Actions.sequence(Actions.fadeOut(0.5f), Actions.run(new Runnable() {
                     @Override
                     public void run() {
+                        if (!nameField.isDisabled()) { //prevent saving the placeholder text as a name
+                            player.setName(nameField.getText());
+                            player.save();
+                        }
                         ((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenu());
                     }
                 })));
@@ -123,15 +160,38 @@ public class Settings implements Screen {
         back.pad(3f, 10f, 3f, 10f);
 
         //putting stuff together
-        table.add("Settings").colspan(3).expandX().spaceBottom(25).row();
+        table.add("Settings", "default").colspan(3).expandX().spaceBottom(25).row();
 
         table.add(back).size(210f, 76f).uniformX().bottom().left().padLeft(28).padBottom(20);
-        table.add().expand().top();
+        table.add(settingsTable).expand().fillX().top();
         table.add(save).size(210f, 76f).uniformX().bottom().right().padRight(28).padBottom(20);
 
         stage.addActor(table);
 
-        stage.addAction(Actions.sequence(Actions.alpha(0), Actions.fadeIn(0.5f)));
+        stage.addAction(Actions.alpha(0)); //set alpha to 0
+        stage.addAction(Actions.fadeIn(0.5f));
+
+        player = Player.getExistingPlayer();
+        player.fetch(new StackMobCallback() {
+
+            @Override
+            public void success(String responseBody) { //wait for success to fade in
+                if (Config.debug) {
+                    Gdx.app.log("StackMob", "Refreshed player data for " + player.getName());
+                }
+                nameField.setText(player.getName());
+                nameField.setDisabled(false);
+            }
+
+            @Override
+            public void failure(StackMobException e) { //otherwise go back to main
+                if (Config.debug) {
+                    Gdx.app.debug("StackMob",
+                            "Error retrieving player information for " + player.getID() + ": " + e.getMessage());
+                }
+                ((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenu());
+            }
+        });
     }
 
     @Override
